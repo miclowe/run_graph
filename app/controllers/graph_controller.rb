affa67743b3d4697f7256e5800d5c6b5258866e0
Michelle Lowe <mick_571@yahoo.com>
2013-05-17 23:20:56 -0700

    Add controller test for index

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -1,2 +1,6 @@
 class GraphController < ApplicationController
+
+  def index
+  end
+
 end
--- spec/controllers/graph_controller_spec.rb
+++ spec/controllers/graph_controller_spec.rb
@@ -2,4 +2,10 @@ require 'spec_helper'
 
 describe GraphController do
 
+  describe "index" do
+    it "responds successfully" do
+      expect(response).to be_success
+    end
+  end
+
 end
