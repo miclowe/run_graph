94af769559d0c96efb62fb180d282b39ab1fc1d0
Michelle Lowe <mick_571@yahoo.com>
2013-05-17 23:16:20 -0700

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ RunGraph::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  root :to => "graph#index"
+  get '/statusboard.json' => 'graph#statusboard'
+  get '/morris.json' => 'graph#morris'
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
