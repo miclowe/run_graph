ea1d9fbd062e22e886b3e1a2b41a08e4042f2c0d
Michelle Lowe <mick_571@yahoo.com>
2013-05-19 02:01:56 -0700

    Test statusboard method in controller

--- spec/controllers/graph_controller_spec.rb
+++ spec/controllers/graph_controller_spec.rb
@@ -8,4 +8,25 @@ describe GraphController do
     end
   end
 
+  describe "statusboard" do
+
+    before(:each) do
+      get :statusboard
+      @body = JSON.parse(response.body)
+    end
+
+    it "responds successfully to statusboard.json" do
+      expect(response).to be_success
+    end
+
+    it "returns a data hash" do
+      expect(@body).to be_a Hash
+    end
+
+    it "returns a data hash that contains a graph key" do
+      expect(@body["graph"]).to be_a Hash
+    end
+
+  end
+
 end
