505501c51b51558bf5f2d053854259174bdf8d7b
Michelle Lowe <mick_571@yahoo.com>
2013-05-19 02:52:31 -0700

    Test morris method in controller

--- spec/controllers/graph_controller_spec.rb
+++ spec/controllers/graph_controller_spec.rb
@@ -29,4 +29,30 @@ describe GraphController do
 
   end
 
+  describe "morris" do
+
+    before(:each) do
+      get :morris
+      @body = JSON.parse(response.body)
+    end
+
+    it "responds successfully to morris.json" do
+      expect(response).to be_success
+    end
+
+    it "returns an Array" do
+      expect(@body).to be_an Array
+    end
+
+    it "returns an Array of hashes" do
+      expect(@body[0]).to be_a Hash
+    end
+
+    it "returns total distance per month" do
+    expect(@body[0]["title"]).to eq "January"
+    expect(@body[0]["value"]).to eq 60.19
+  end
+
+  end
+
 end
